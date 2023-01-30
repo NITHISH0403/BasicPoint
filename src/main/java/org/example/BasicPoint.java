fe368f632a0d7bfac13ec6ebdeaa024779612098
NITHISH KUMAR M <123449276+NITHISH0403@users.noreply.github.com>
2023-01-30 12:25:11 +0530

    Update BasicPoint.java

--- src/main/java/org/example/BasicPoint.java
+++ src/main/java/org/example/BasicPoint.java
@@ -37,7 +37,7 @@ class BasicPoint {
         Point o1 = new Point();
         Point o2 = o1.clonePoint();
 
-        l.info(o1.x1 + " " + o1.y1 );
+        l.log(Level.INFO, () ->o1.x1 + " " + o1.y1 );
         if(o1.x1.equals(o2.x2) && o1.y1.equals(o2.y2)){
             l.info("Your Co-ordinates is True.");
             l.log(Level.INFO, () ->"x1:" + o1.x1 + " " + "y1:" + o1.y1);
